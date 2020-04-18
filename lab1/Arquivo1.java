5d98f23bb44d7cfc6906a300852e8e0fa0fb1202
João Bosco de Barros Freitas <jbbfreitas@gmail.com>
2020-04-18 18:27:36 -0400

    commit inical

---
+++ lab1/Arquivo1.java
@@ -0,0 +1,6 @@
+package lab1;
+public class Arquivo1 {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo! Este é minha primeira classe java modificada");
+    }
+}
