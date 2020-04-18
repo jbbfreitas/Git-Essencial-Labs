f5e8cb019b90018843f9fb0a52e5cafc22748dd6
João Bosco de Barros Freitas <jbbfreitas@gmail.com>
2020-04-18 18:30:24 -0400

    Adicionada mais uma linha no commit anterior

--- lab1/Arquivo1.java
+++ lab1/Arquivo1.java
@@ -1,6 +1,7 @@
 package lab1;
 public class Arquivo1 {
     public static void main(String[] args) {
-        System.out.println("Olá mundo! Este é minha primeira classe java modificada");
+        System.out.println("Olá mundo! Este é minha primeira classe modificada");
+        System.out.println("Esta linha não havia sido incluída");
     }
 }
