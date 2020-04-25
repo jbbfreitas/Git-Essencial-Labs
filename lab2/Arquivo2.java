7888961092408153819bc99867def58902f0d7e4
João Bosco de Barros Freitas <jbbfreitas@gmail.com>
2020-04-25 16:30:28 -0400

    Voltando o Arquivo2.java para o original

--- lab2/Arquivo2.java
+++ lab2/Arquivo2.java
@@ -13,9 +13,8 @@ public class Arquivo2 {
 
         int a = 500;
         int b = 1000;
-        int c = 1500;
-
-        System.out.println("a+b+c="+(a+b+c));
+     
+        System.out.println("a+b="+(a+b));
          
 
 
